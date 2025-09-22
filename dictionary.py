738b8576dd717632378bd48dd901c8199aa65306
omkarchavan1a <128473723+omkarchavan1a@users.noreply.github.com>
2025-09-23 01:14:58 +0530

    more added

--- dictionary.py
+++ dictionary.py
@@ -1,39 +1,74 @@
-dict = {"omkar":23,"mahesh":24,"lakhan":25,"hemesh":26,"pratik":27}
+my_dict = {"omkar":23,"mahesh":24,"lakhan":25,"hemesh":26,"pratik":27}
 
-print(dict)
+print(my_dict)
 
-dict["ram"]= 90
-print(dict)
+my_dict["ram"]= 90
+print(my_dict)
 
-dict["lakhan"] = 80
-print(dict)
+my_dict["lakhan"] = 80
+print(my_dict)
 
-del dict["pratik"]
-print(dict)
+del my_dict["pratik"]
+print(my_dict)
 
-if "lakhan" in dict:
+if "lakhan" in my_dict:
     print("the lakhan is present in the dictionary")
 else:
     print("not present in dictionary")    
     
-for i in dict:
+for i in my_dict:
     print(i)    
     
-for i in dict.values():
+for i in my_dict.values():
     print(i)    
     
-for i , j in dict .items():
+for i , j in my_dict .items():
         print(f"{i}:{j}")  
 
-print(len(dict))          
+print(len(my_dict))          
 
-dict.update({"sumit":23,"rohan":40})
-print(dict)
+my_dict.update({"sumit":23,"rohan":40})
+print(my_dict)
 
-# .Create a dictionary from two lists: one of keys and one of values.
+
 keys = ["a", "b", "c", "d", "e"]
 values = [1, 2, 3, 4, 5]
 dict1 = dict(zip(keys, values))
 print(dict1)
 
+sorted_keys = dict(sorted(my_dict.items(), key=lambda x: x[0]))
+print(sorted_keys)
+sorted_items = dict(sorted(my_dict.items(), key=lambda x: x[1]))
+print(f" {sorted_items}")
 
+highest =max(my_dict.values())
+print(highest)
+
+lowest = min(my_dict.values())
+print(lowest)
+
+sentence = "programming"
+char_frequency ={}
+for i in sentence:
+    char_frequency[i] = char_frequency.get(i,0) + 1
+print(char_frequency)
+
+square_dict = {key: key**2 for key in range(1,11)}
+print(square_dict)
+
+reversed_dict = {num : str(num)[::-1] for num in range(1,11)}
+print(reversed_dict)
+
+student_info = {
+    "student1":{"name":"omkar","age":23,"gender":"male"},
+    "student2":{"name":"pratik","age":21,"gender":"male"}   
+    
+}
+print(student_info)
+
+sentence = "programers house"
+word = sentence.split()
+frequency = {}
+for i in sentence:
+    frequency[i] = frequency.get(i,0) + 1
+print(frequency)
